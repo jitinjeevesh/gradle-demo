24d3f11be5a2765bd2991f9fbc392a7b590ed1b4
Jeevesh Pandey <jpandey@dminc.com>
2016-11-16 14:12:47 +0530

    first commit

---
+++ source/main/java/com/Application.java
@@ -0,0 +1,12 @@
+package com;
+
+/**
+ * Created by oem on 16/11/16.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("........................");
+        System.out.println("Hello inside main class");
+    }
+}
